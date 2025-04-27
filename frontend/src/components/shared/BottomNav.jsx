6133fe00cdb13d83715b1f916728e83dd85a45c2
Sugavanesh17 <sugavaneshwaran.s2023cse@sece.ac.in>
2025-04-27 13:25:26 +0530

    Create order

--- frontend/src/components/shared/BottomNav.jsx
+++ frontend/src/components/shared/BottomNav.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaHome } from "react-icons/fa";
 import { MdOutlineReorder, MdTableBar } from "react-icons/md";
 import { CiCircleMore } from "react-icons/ci";
 import { BiSolidDish } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
+import Modal from "./Modal";
 
 const BottomNav = () => {
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModel= () => { setIsModalOpen(true)};
+  const closeModal= () => { setIsModalOpen(false)};
+  const [guestCount, setGuestCount] = useState(0);
+  const [name, setName] = useState();
+  const [phone, setPhone] = useState();
+
+  const increment = () => {
+    if(guestCount >= 6) return;
+    setGuestCount((prev) => prev + 1);
+  }
+  const decrement = () => {
+    if(guestCount <= 0) return;
+    setGuestCount((prev) => prev - 1);
+  }
+
+  const isActive = (path) => location.pathname === path;
+
+  const handleCreateOrder = () => {
+    // send the data to store
+    dispatch(setCustomer({name, phone, guests: guestCount}));
+    navigate("/tables");
+  }
+
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[#262626] p-2 h-16 flex justify-around">
       <button onClick={()=>navigate("/")} className="flex items-center justify-center text-[#F5F5F5] bg-[#343434] w-[200px] rounded-[20px]">
@@ -21,9 +46,34 @@ const BottomNav = () => {
       <button className="flex items-center justify-center text-[#ababa] w-[200px]">
         <CiCircleMore className="inline mr-2" size={20} /> <p>More</p>
       </button>
-      <button className="absolute bottom-3 bg-[#F6B100] text-[#F5F5F5] rounded-full p-3 items-center">
-        <BiSolidDish size={30} />
+      <button onClick={openModel} className="absolute bottom-3 bg-[#F6B100] text-[#F5F5F5] rounded-full p-3 items-center">
+        <BiSolidDish size={40} />
       </button>
+      <Modal isOpen={isModalOpen} onClose={closeModal} title="Create Order">
+        <div>
+          <label className="block text-[#ababab] mb-2 text-sm font-medium">Customer Name</label>
+          <div className="flex items-center rounded-lg p-3 px-4 bg-[#1f1f1f]">
+            <input value={name} onChange={(e) => setName(e.target.value)} type="text" name="" placeholder="Enter customer name" id="" className="bg-transparent flex-1 text-white focus:outline-none"  />
+          </div>
+        </div>
+        <div>
+          <label className="block text-[#ababab] mb-2 mt-3 text-sm font-medium">Customer Phone</label>
+          <div className="flex items-center rounded-lg p-3 px-4 bg-[#1f1f1f]">
+            <input value={phone} onChange={(e) => setPhone(e.target.value)} type="number" name="" placeholder="+91-9999999999" id="" className="bg-transparent flex-1 text-white focus:outline-none"  />
+          </div>
+        </div>
+        <div>
+          <label className="block mb-2 mt-3 text-sm font-medium text-[#ababab]">Guest</label>
+          <div className="flex items-center justify-between bg-[#1f1f1f] px-4 py-3 rounded-lg">
+            <button onClick={decrement} className="text-yellow-500 text-2xl">&minus;</button>
+            <span className="text-white">{guestCount} Person</span>
+            <button onClick={increment} className="text-yellow-500 text-2xl">&#43;</button>
+          </div>
+        </div>
+        <button onClick={()=>navigate("/tables")} className="w-full bg-[#F6B100] text-[#f5f5f5] rounded-lg py-3 mt-8 hover:bg-yellow-700">
+          Create Order
+        </button>
+      </Modal>
     </div>
   );
 };
