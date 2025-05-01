f684d69c7053acb166b8576080588276a239229f
Sugavanesh17 <sugavaneshwaran.s2023cse@sece.ac.in>
2025-05-01 21:18:24 +0530

    Orders updataion

--- frontend/src/components/orders/OrderCard.jsx
+++ frontend/src/components/orders/OrderCard.jsx
@@ -1,40 +1,56 @@
 import React from "react";
-import { FaCheckDouble } from "react-icons/fa";
+import { FaCheckDouble, FaLongArrowAltRight } from "react-icons/fa";
 import { FaCircle } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import { formatDateAndTime, getAvatarName } from "../../utils/index";
 
-const OrderCard = () => {
+const OrderCard = ({ key, order }) => {
+  console.log(order);
   return (
-    <div className="w-[500px] bg-[#262626] p-4 rounded-lg mb-4">
-      <div className="flex items-center gap-3">
+    <div key={key} className="w-[500px] bg-[#262626] p-4 rounded-lg mb-4">
+      <div className="flex items-center gap-5">
         <button className="bg-[#f6b100] p-3 text-xl font-bold rounded-lg">
-          AM
+          {getAvatarName(order.customerDetails.name)}
         </button>
         <div className="flex items-center justify-between w-[100%]">
-          <div className="flex flex-col items-end gap-1">
-            <h1 className="text-[#F5F5F5] text-lg font-semibold tracking-wide">
-              Amrit Raj
+          <div className="flex flex-col items-start gap-1">
+            <h1 className="text-[#f5f5f5] text-lg font-semibold tracking-wide">
+              {order.customerDetails.name}
             </h1>
-            <p className="text-[#ababab] text-sm">#101/Dine In</p>
+            <p className="text-[#ababab] text-sm">#{Math.floor(new Date(order.orderDate).getTime())} / Dine in</p>
+            <p className="text-[#ababab] text-sm">Table <FaLongArrowAltRight className="text-[#ababab] ml-2 inline" /> {order.table.tableNo}</p>
           </div>
-          <div className="flex flex-col items-start gap-2">
-            <p className="text-green-600 bg-[#2e4a40] px-2 py-1 rounded-lg">
-              <FaCheckDouble className="inline mr-2" /> Ready
-            </p>
-            <p className="text-[#ababab] text-sm">
-              <FaCircle className="inline mr-2 text-green-600" /> Ready to serve
-            </p>
+          <div className="flex flex-col items-end gap-2">
+            {order.orderStatus === "Ready" ? (
+              <>
+                <p className="text-green-600 bg-[#2e4a40] px-2 py-1 rounded-lg">
+                  <FaCheckDouble className="inline mr-2" /> {order.orderStatus}
+                </p>
+                <p className="text-[#ababab] text-sm">
+                  <FaCircle className="inline mr-2 text-green-600" /> Ready to
+                  serve
+                </p>
+              </>
+            ) : (
+              <>
+                <p className="text-yellow-600 bg-[#4a452e] px-2 py-1 rounded-lg">
+                  <FaCircle className="inline mr-2" /> {order.orderStatus}
+                </p>
+                <p className="text-[#ababab] text-sm">
+                  <FaCircle className="inline mr-2 text-yellow-600" /> Preparing your order
+                </p>
+              </>
+            )}
           </div>
         </div>
       </div>
       <div className="flex justify-between items-center mt-4 text-[#ababab]">
-        <p>January 18, 2025 08:32 PM </p>
-        <p>8 items</p>
+        <p>{formatDateAndTime(order.orderDate)}</p>
+        <p>{order.items.length} Items</p>
       </div>
       <hr className="w-full mt-4 border-t-1 border-gray-500" />
       <div className="flex items-center justify-between mt-4">
         <h1 className="text-[#f5f5f5] text-lg font-semibold">Total</h1>
-        <p className="text-[#f5f5f5] text-lg font-semibold">$ 250.00</p>
+        <p className="text-[#f5f5f5] text-lg font-semibold">₹{order.bills.totalWithTax.toFixed(2)}</p>
       </div>
     </div>
   );
