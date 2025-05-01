bdae880906eda1b487c7c0f6eea9c76c29ee8623
Sugavanesh17 <sugavaneshwaran.s2023cse@sece.ac.in>
2025-05-01 10:50:27 +0530

    Login and register UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {Home, Auth, Orders, Menu} from './pages';
-import Header from './components/shared/Header';
-import Tables from './pages/Tables';
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { Home, Auth, Orders, Menu } from "./pages";
+import Header from "./components/shared/Header";
+import Tables from "./pages/Tables";
+
+function Layout() {
+  const location = useLocation();
+  const hideHeaderRoutes = ["/auth"];
+
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/tables" element={<Tables />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
-        </Routes>
-      </Router>
+      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/tables" element={<Tables />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
     </>
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <Layout />
+    </Router>
   );
 }
 
